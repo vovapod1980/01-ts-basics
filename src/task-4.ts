48035b5cc80db5830a650af103ae40454990903a
Volodymyr Podoliako <vpodol1980@gmail.com>
2026-04-14 18:24:19 +0300

    add 5 tasks

---
+++ src/task-4.ts
@@ -0,0 +1,20 @@
+// Задача 4. Функції (файл task-4.ts)
+// Функція printUserInfo виводить інформацію про користувача, включаючи ім’я, вік і (опціонально) email.
+
+function printUserInfo(name: string, age: number, email?: string): void {
+  console.log("Name:", name);
+  console.log("Age:", age);
+  if (email) {
+    console.log("Email:", email);
+  }
+}
+
+printUserInfo("Alice", 30);
+printUserInfo("Bob", 25, "bob@mail.com");
+
+// Завдання:
+
+// Додай явну типізацію до параметрів функції: name, age та email.
+// Зроби параметр email опціональним – щоб функція могла працювати як з ним, так і без нього.
+// Типізуй повернення функції, зауваж що вона нічого явно не повертає.
+// Залиш реалізацію функції без змін.
